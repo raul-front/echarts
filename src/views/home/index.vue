e7e58bde5f51352f8063626c5a0a34d1f2ca4df8
raul <18919015125@163.com>
2021-07-02 19:15:29 +0800

    add: full screen

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -6,30 +6,58 @@
       </template>
       <el-row :gutter="12">
         <el-col :span="6" v-for="item in largeScreenList" :key="item.id">
-          <router-link :to="{name: item.routeName}">
+          <div @click="openScreenPage(item)">
             <el-card shadow="always">
               <template #header>
                   <span>{{item.title}}</span>
               </template>
               <img class="large-screen-item-img" :src="item.img" alt="">
             </el-card>
-          </router-link>
+          </div>
         </el-col>
       </el-row>
     </el-card>
   </div>
 </template>
 <script>
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, computed } from 'vue'
+import { useRouter } from 'vue-router'
 
 export default defineComponent({
   setup () {
+    const router = useRouter()
+
+    const isFullScreen = computed(() => {
+      return window.navigator.userAgent.indexOf('MSIE') < 0
+    })
+    const handleFullScreen = () => {
+      if (!isFullScreen.value) return
+
+      const main = document.body
+      if (main.requestFullscreen) {
+        main.requestFullscreen()
+      } else if (main.mozRequestFullScreen) {
+        main.mozRequestFullScreen()
+      } else if (main.webkitRequestFullScreen) {
+        main.webkitRequestFullScreen()
+      } else if (main.msRequestFullscreen) {
+        main.msRequestFullscreen()
+      }
+    }
+
     const largeScreenList = ref([
       // 为了打包后能方法，加上 publicDir
       { id: 1, title: '监控大屏1', routeName: 'Large_Screen_1', img: '/echarts/images/large-screen-page/large-screen-1.png' },
     ])
+
+    const openScreenPage = (item) => {
+      handleFullScreen()
+      router.push({ name: item.routeName })
+    }
+
     return {
       largeScreenList,
+      openScreenPage,
     }
   },
 })
